6d9bcbdf3459bf3ffe27475b8d6a8e33bdc6ad00
Sonu Lohani <sonulohani@gmail.com>
2020-07-01 20:45:37 +0530

    Add command line parser

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,28 @@
+#include <QCommandLineParser>
 #include <QCoreApplication>
+
+#include "config.h"
+
+void initCommandLineParser(const QCoreApplication &application)
+{
+    QCommandLineParser parser;
+    parser.setApplicationDescription(QObject::tr("Terminal media player"));
+    parser.addHelpOption();
+    parser.addVersionOption();
+    QCommandLineOption inputOption(QStringList() << "i"
+                                                 << "input",
+                                   QObject::tr("Input to the tool"));
+    parser.addOption(inputOption);
+    parser.process(application);
+}
 
 auto main(int argc, char *argv[]) -> int
 {
     QCoreApplication app(argc, argv);
+    QCoreApplication::setApplicationName(PROJECT_NAME);
+    QCoreApplication::setApplicationVersion(PROJECT_VER);
+
+    initCommandLineParser(app);
+
     return QCoreApplication::exec();
 }
