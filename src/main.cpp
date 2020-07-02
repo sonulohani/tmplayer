323d3bc216fe2c6760a33ba0dff75902b6f5d510
Sonu Lohani <sonulohani@gmail.com>
2020-07-02 10:44:37 +0530

    Add license

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,9 @@
+/*
+ * Copyright (C) 2020-present, Sonu Lohani & tmplayer contributors.
+ * Please read the license file provided with the tool or click on
+ * this link : https://github.com/sonulohani/tmplayer/blob/master/LICENSE
+ */
+
 #include <QCommandLineParser>
 #include <QCoreApplication>
 
